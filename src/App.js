bfc501283c93e950a915b23e84ee746effaaf0b4
joshuaobare <jobare.obare@gmail.com>
2022-08-29 17:21:57 +0300

    project initialization

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React , {Component} from 'react';
+
+class App extends Component {
+  constructor() {
+    super()
+  }
+
+
+
+  render(){
+    return (
+      <div className="App">
+        <h1> Title</h1>
+      </div>
+    );
+  }
+  
+}
+
+export default App;
