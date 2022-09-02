38adb3997fdd4c5b04a76c15e8074ea1b58f096f
joshuaobare <jobare.obare@gmail.com>
2022-09-02 20:49:51 +0300

    separated state arrays based on components. added title state

--- src/App.js
+++ src/App.js
@@ -12,41 +12,53 @@ class App extends Component {
 
     this.state = {
       name:{text: "",
-    id:uniqid()},
-      names : [],
+      id:uniqid(),
+    title:"Name"},
+      info : [],
       email:{text: "",
-      id:uniqid()},
-      emails : [],
+      id:uniqid(),
+    title:"Email"},
+      
       number : {text: "",
-      id:uniqid()}, 
-      numbers : [],    
+      id:uniqid(),
+    title:"Number"}, 
+          
       school:{text: "",
-      id:uniqid()},
-      schools : [],
+      id:uniqid(),
+    title:"School"},
+      education : [],
       degree:{text: "",
-      id:uniqid()},
-      degrees : [],
+      id:uniqid(),
+    title:"Degree"},
+      
       schoolStart:{text: "",
-      id:uniqid()},
-      schoolStarts : [],
+      id:uniqid(),
+    title:"Start Date"},
+      
       schoolEnd : {text: "",
-      id:uniqid()},
-      schoolEnds:[],
+      id:uniqid(),
+    title:"End Date"},
+      
       company : {text: "",
-      id:uniqid()},
-      companies: [],     
+      id:uniqid(),
+    title:"Company"},
+      experience: [],     
       position:{text: "",
-      id:uniqid()},
-      positions:[],
+      id:uniqid(),
+    title:"Position"},
+      
       roles:{text: "",
-      id:uniqid()},
-      role:[],
+      id:uniqid(),
+    title:"Roles"},
+      
       workStart:{text: "",
-      id:uniqid()},
-      workStarts:[],
+      id:uniqid(),
+    title:"Start Date"},
+     
       workEnd : {text: "",
-      id:uniqid()},
-      workEnds:[],
+      id:uniqid(),
+    title: "End Date"}
+      
     
     
     }
@@ -58,40 +70,40 @@ class App extends Component {
   handleChange = (e) => {
     this.setState({
       name: {
-        text: document.querySelector("#name").value , id: this.state.name.id
+        text: document.querySelector("#name").value , id: this.state.name.id, title:this.state.name.title
       },
       email: {
-        text: document.querySelector("#email").value, id: this.state.email.id
+        text: document.querySelector("#email").value, id: this.state.email.id, title:this.state.email.title
       },
       number: {
-        text: document.querySelector("#number").value, id: this.state.number.id
+        text: document.querySelector("#number").value, id: this.state.number.id, title:this.state.number.title
       },
       school: {
-        text: document.querySelector("#school").value, id: this.state.school.id
+        text: document.querySelector("#school").value, id: this.state.school.id, title:this.state.school.title
       },
       degree: {
-        text: document.querySelector("#degree").value, id: this.state.degree.id
+        text: document.querySelector("#degree").value, id: this.state.degree.id, title:this.state.degree.title
       },
       schoolStart: {
-        text: document.querySelector("#schoolStart").value, id: this.state.schoolStart.id
+        text: document.querySelector("#schoolStart").value, id: this.state.schoolStart.id, title:this.state.schoolStart.title
       },
       schoolEnd: {
-      text: document.querySelector("#schoolEnd").value , id: this.state.schoolEnd.id
+      text: document.querySelector("#schoolEnd").value , id: this.state.schoolEnd.id, title:this.state.schoolEnd.title
       },
       company: {
-        text: document.querySelector("#company").value, id: this.state.company.id
+        text: document.querySelector("#company").value, id: this.state.company.id, title:this.state.company.title
       },
       position: {
-        text: document.querySelector("#position").value, id: this.state.position.id
+        text: document.querySelector("#position").value, id: this.state.position.id, title:this.state.position.title
       },
       roles: {
-        text: document.querySelector("#roles").value, id: this.state.roles.id
+        text: document.querySelector("#roles").value, id: this.state.roles.id, title:this.state.roles.title
       },
       workStart: {
-        text: document.querySelector("#workStart").value, id: this.state.workStart.id
+        text: document.querySelector("#workStart").value, id: this.state.workStart.id, title:this.state.workStart.title
       },
       workEnd: {
-      text: document.querySelector("#workEnd").value , id: this.state.workEnd.id
+      text: document.querySelector("#workEnd").value , id: this.state.workEnd.id, title:this.state.workEnd.title
       }
       
     })
@@ -100,39 +112,30 @@ class App extends Component {
   onSubmitName = (e) => {
     e.preventDefault()
     this.setState({
-      names: this.state.names.concat(this.state.name),
-      emails:this.state.emails.concat(this.state.email),
-      numbers:this.state.numbers.concat(this.state.number),
-      schools:this.state.schools.concat(this.state.school),
-      degrees:this.state.degrees.concat(this.state.degree),
-      schoolStarts:this.state.schoolStarts.concat(this.state.schoolStart),
-      schoolEnds:this.state.schoolEnds.concat(this.state.schoolEnd),
-      companies:this.state.companies.concat(this.state.company),
-      positions:this.state.positions.concat(this.state.position),
-      role:this.state.role.concat(this.state.roles),
-      workStarts:this.state.workStarts.concat(this.state.workStart),
-      workEnds:this.state.workEnds.concat(this.state.workEnd),
-      name:{text: "",id : uniqid()},
-      email:{text: "",id : uniqid()},
-      number : {text: "",id : uniqid()},     
-      school:{text: "",id : uniqid()},
-      degree:{text: "",id : uniqid()},
-      schoolStart:{text: "",id : uniqid()},
-      schoolEnd : {text: "",id : uniqid()},
-      company : {text: "",id : uniqid()},     
-      position:{text: "",id : uniqid()},
-      roles:{text: "",id : uniqid()},
-      workStart:{text: "",id : uniqid()},
-      workEnd : {text: "",id : uniqid()}
+      
+      info: this.state.info.concat(this.state.name,this.state.email,this.state.number),      
+      education:this.state.education.concat(this.state.school,this.state.degree,this.state.schoolStart,this.state.schoolEnd),
+      experience:this.state.experience.concat(this.state.company,this.state.position,this.state.roles,this.state.workStart,this.state.workEnd),
+      name:{text: "",id : uniqid(),title:"Name"},
+      email:{text: "",id : uniqid(),title:"Email"},
+      number : {text: "",id : uniqid(),title:"Number"},     
+      school:{text: "",id : uniqid(),title:"School"},
+      degree:{text: "",id : uniqid(),title:"Degree"},
+      schoolStart:{text: "",id : uniqid(),title:"Start Date"},
+      schoolEnd : {text: "",id : uniqid(),title:"End Date"},
+      company : {text: "",id : uniqid(),title:"Company"},     
+      position:{text: "",id : uniqid(),title:"Position"},
+      roles:{text: "",id : uniqid(),title:"Roles"},
+      workStart:{text: "",id : uniqid(),title:"Start Date"},
+      workEnd : {text: "",id : uniqid(),title:"End Date"}
     })
   }
 
 
 
   render() {
-
-    const {name,names,email,emails,number,numbers, degree, degrees, school,schools,schoolStart,schoolStarts,schoolEnd,schoolEnds,
-    company,companies,position,positions,roles,role,workStart,workStarts,workEnd,workEnds } = this.state
+    const {name,email,number,degree,school,schoolStart,schoolEnd,
+      company,position,roles,workStart,workEnd , info, experience,education } = this.state
 
     return (
       <div className="App">
@@ -140,8 +143,8 @@ class App extends Component {
         <General onChange={this.handleChange} nameValue={name.text} emailValue={email.text} numberValue={number.text} onSubmitName={this.onSubmitName}  />
         <Education onChange={this.handleChange} schoolName={school.text} degreeName={degree.text} schoolStart={schoolStart.text} schoolEnd={schoolEnd.text} onSubmitName={this.onSubmitName} />
         <Experience onChange={this.handleChange} company={company.text} position={position.text} roles={roles.text} workStart={workStart.text} workEnd={workEnd.text} onSubmitName={this.onSubmitName}/>
-        <Overview names = {names} email={emails} number={numbers} school={schools} degree={degrees} schoolStart={schoolStarts} schoolEnd={schoolEnds}
-        company={companies} position={positions} roles={role} workStart={workStarts} workEnd={workEnds}/>
+        <Overview info = {info} experience = {experience} education = {education} />
+        
       </div>
     );
   }
--- src/components/Overview.js
+++ src/components/Overview.js
@@ -4,134 +4,48 @@ import React , {Component} from 'react';
 class Overview extends Component {
     
     render(){
-        const {names,email,number,school,degree,schoolStart,schoolEnd, company,position,roles,workStart,workEnd} = this.props
+        const {info,education,experience} = this.props
 
-        console.log(names)
         return (
             <div>
                 <h1>Details</h1>
 
-                {names.map((name)=> {
-                    if (name.text === ""){
-                        return null
-                    } else {
-                        return <p key={name.id}>Name: {name.text}</p>
-                    }
+                {
+                    info.map((data,i) => {
+                        if (data.text === "") {
+                            return null
+                        }else {
+                          return <p key={data.id}>{data.title}: {data.text}</p>}
+                        
+                    })
+                }
 
-                    
-                })}
-
-                {email.map((emails)=> {
-
-                    if (emails.text === ""){
-                        return null
-                    } else {
-                        return <p key={emails.id}>Email: {emails.text}</p>
-                    }
-
-                    
-                })}
-                {number.map((numbers)=> {
-
-                    if (numbers.text === ""){
-                        return null
-                    } else {
-                        return<p key={numbers.id}>Phone Number: {numbers.text}</p>
-                    }
-
-                    
-                })}
 
                 <h2>Education</h2>
 
-                {school.map((schools)=> {
-
-                    if (schools.text === ""){
-                        return null
-                    } else {
-                        return <p key={schools.id}>School: {schools.text}</p>
-                    }
-
-                    
-                })}
-
-                {degree.map((degrees)=> {
-
-                    if (degrees.text === ""){
-                        return null
-                    } else {
-                        return <p key={degrees.id}>Degree: {degrees.text}</p>
-                    }                    
-                })}
-
-                {schoolStart.map((schoolStarts)=> {
-
-                    if (schoolStarts.text === ""){
-                        return null
-                    } else {
-                        return <p key={schoolStarts.id}>Start Date: {schoolStarts.text}</p>
-                    }  
-                })}
-
-                {schoolEnd.map((schoolEnds)=> {
-
-                    if (schoolEnds.text === ""){
-                        return null
-                    } else {
-                        return <p key={schoolEnds.id}>End Date: {schoolEnds.text}</p>
-                    }                    
-                })}
+                {
+                    education.map((data,i) => {
+                        if(data.text === "") {
+                            return null
+                        } else {
+                            return <p key={data.id}>{data.title}: {data.text}</p>
+                        } 
+                    })
+                }
 
                 <h2>Work Experience</h2>
-                {company.map((companies)=> {
 
-                    if (companies.text === ""){
-                        return null
-                    } else {
-                        return <p key={companies.id}>Company: {companies.text}</p>
-                    }
-                    
-                })}
+                {
+                    experience.map((data,i) => {
+                        if(data.text === "") {
+                            return null
+                        } else {
+                            return <p key={data.id}>{data.title}: {data.text}</p>
+                        }                                     
+                        
+                    })
+                }                              
 
-                {position.map((positions)=> {
-
-                    if (positions.text === ""){
-                        return null
-                    } else {
-                        return <p key={positions.id}>Position: {positions.text}</p>
-                    }
-
-                    
-                })}
-                {roles.map((role)=> {
-
-                    if (role.text === ""){
-                        return null
-                    } else {
-                        return <p key={role.id}>Roles: {role.text}</p>
-                    }
-
-                    
-                })}
-
-                {workStart.map((workStarts)=> {
-
-                    if (workStarts.text === ""){
-                        return null
-                    } else {
-                        return <p key={workStarts.id}>Start Date: {workStarts.text}</p>
-                    }                    
-                })}
-
-                {workEnd.map((workEnds)=> {
-
-                    if (workEnds.text === ""){
-                        return null
-                    } else {
-                        return <p key={workEnds.id}>End Date: {workEnds.text}</p>
-                    }                    
-                })}                 
-                
             </div>
         )
     }
